f646503182598f7840db29af04bc7301f511fe1c
pawcio <pawelzarzycki89@gmail.com>
2021-03-14 23:25:51 +0100

    login page setup

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 3001;
 const middleware = require('./middleware')
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
-
 const server = app.listen(port, () => {
     console.log("Serwer listen on port " + port)
 });
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+//Routes
+const loginRoutes = require('./routes/loginRoutes')
+app.use("/login", loginRoutes)
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
