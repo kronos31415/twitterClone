9266fc05bc8f7949e17a1eb8bcee64b38f26b425
pawcio <pawelzarzycki89@gmail.com>
2021-03-14 22:48:40 +0100

    login middleware

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-var http = require('http');
 const app = express();
 const port = 3001;
+const middleware = require('./middleware')
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
@@ -10,7 +10,7 @@ const server = app.listen(port, () => {
     console.log("Serwer listen on port " + port)
 });
 
-app.get("/", (req, res, next) => {
+app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
         title: 'Welocme'
     }
