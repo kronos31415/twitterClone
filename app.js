3847284383db0d8aaccc2b90c61c5fcc3048c8ba
pawcio <pawelzarzycki89@gmail.com>
2021-03-15 23:13:16 +0100

    login register forms

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const middleware = require('./middleware')
+const path = require('path')
 
 const server = app.listen(port, () => {
     console.log("Serwer listen on port " + port)
@@ -9,10 +10,14 @@ const server = app.listen(port, () => {
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 const loginRoutes = require('./routes/loginRoutes')
 app.use("/login", loginRoutes)
+
+const registerRoutes = require('./routes/registerRoutes')
+app.use("/register", registerRoutes)
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
